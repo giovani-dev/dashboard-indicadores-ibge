6db2d07c714dd508370dbf4cf7b2b8807f5a4672
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2025-05-14 17:01:07 -0300

    .

--- src/main.py
+++ src/main.py
@@ -32,6 +32,10 @@ st.set_page_config(layout="wide")
 custo_medio_m2 = pd.read_csv("./static/custo-medio-m2-em-moeda-corrente.csv")
 # https://sidra.ibge.gov.br/tabela/1737
 ipca = pd.read_csv("./static/IPCA-serie-histórica.csv")
+# https://sidra.ibge.gov.br/tabela/5438
+rendimento_mensal = pd.read_csv(
+    "./static/rendimento-medio-mensal-real-trabalhadores.csv"
+)
 
 custo_medio_m2 = custo_medio_m2.rename(
     columns={
@@ -49,19 +53,32 @@ ipca = ipca.rename(
 )
 
 df = pd.concat([custo_medio_m2, ipca])
+
 df["Data"] = pd.to_datetime(df["Mês"].apply(convert_to_datetime)).dt.date
+df["Data"] = pd.to_datetime(df["Data"], errors="coerce")
 
-start_date = st.sidebar.date_input(
-    "Seleciona a data inicial", value=datetime(year=2020, month=1, day=1)
+df["Trimestre"] = df["Data"].dt.to_period("Q").astype(str)
+
+quarter = st.sidebar.selectbox(
+    "Trimestres disponiveis", df["Trimestre"].dropna().unique(), index=None
 )
-end_date = st.sidebar.date_input("Seleciona a data final", value="today")
-
+start_date = st.sidebar.date_input("Seleciona a data inicial", value=None)
+end_date = st.sidebar.date_input("Seleciona a data final", value=None)
+instruction_level = st.sidebar.selectbox(
+    "Selecione o nivel de instrução",
+    rendimento_mensal[rendimento_mensal["Nível de instrução"] != "Total"][
+        "Nível de instrução"
+    ].unique(),
+    index=None,
+)
+if quarter:
+    df = df[df["Trimestre"] == quarter]
 if start_date and end_date:
-    df = df[(df["Data"] >= start_date) & (df["Data"] <= end_date)]
+    df = df[(df["Data"].dt.date >= start_date) & (df["Data"].dt.date <= end_date)]
 if start_date and not end_date:
-    df = df[df["Data"] >= start_date]
+    df = df[df["Data"].dt.date >= start_date]
 if not start_date and end_date:
-    df = df[df["Data"] <= end_date]
+    df = df[df["Data"].dt.date <= end_date]
 
 
 col1, col2 = st.columns(2)
